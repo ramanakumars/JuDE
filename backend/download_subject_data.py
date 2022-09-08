85381dea3b53dfd6e7896360a8e5df2c70fae12a
Ramanakumar Sankar <rsankar@umn.edu>
2022-09-08 00:53:27 -0500

    fixing issues with sharing new vortex data across workers

--- backend/download_subject_data.py
+++ backend/download_subject_data.py
@@ -5,11 +5,12 @@ with open('/run/secrets/panoptes_username') as ufile:
 with open('/run/secrets/panoptes_password') as pfile:
     password = pfile.read().strip()
 
-Panoptes.connect(username=username, password=password)
+client = Panoptes.connect(username=username, password=password)
 
 project = Project(17032)
 
-export = project.get_export('subjects', generate=True)
+with client:
+    export = project.get_export('subjects')
 
 with open('jvh_subjects.csv', 'w') as out_file:
     out_file.write(export.content.decode('utf-8'))
