e74a822bea581091274526fe6f7566f51c732ee5
Ramanakumar Sankar <rsankar@umn.edu>
2022-09-08 09:41:32 -0500

    fixing cron tab issues with data download

--- backend/refresh_subject_data.sh
+++ backend/refresh_subject_data.sh
@@ -3,6 +3,6 @@
 currtime=$(date);
 echo "Getting new data at ${currtime}";
 
-python3 download_subject_data.py;
+python3 /app/download_subject_data.py;
 
 curl http://localhost:5000/backend/refresh-vortex-list/;
