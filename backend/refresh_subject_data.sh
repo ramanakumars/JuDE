71de18f395470442305e00e607070538424232be
Ramanakumar Sankar <rsankar@umn.edu>
2022-09-08 20:10:38 -0500

    fixing crontab python module dependencies

--- backend/refresh_subject_data.sh
+++ backend/refresh_subject_data.sh
@@ -3,6 +3,8 @@
 currtime=$(date);
 echo "Getting new data at ${currtime}";
 
-python3 /app/download_subject_data.py;
+/usr/local/bin/python3 /app/download_subject_data.py;
 
 curl http://localhost:5000/backend/refresh-vortex-list/;
+
+echo "----------------------------------------------";
