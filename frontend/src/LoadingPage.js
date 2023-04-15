113ad30cdc02ee2ffe703b5d44b30f333cd1ca34
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2023-04-15 18:46:18 -0500

    formatted code

--- frontend/src/LoadingPage.js
+++ frontend/src/LoadingPage.js
@@ -1,48 +1,38 @@
-import React from 'react';
-import './loading.css';
+import React from "react";
+import "./loading.css";
 
 class LoadingPage extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {enabled: props.enable, text: "Loading..."};
+        this.state = { enabled: props.enable, text: "Loading..." };
 
-		this.enable  = this.enable.bind(this);
-		this.disable = this.disable.bind(this);
-	}
+        this.enable = this.enable.bind(this);
+        this.disable = this.disable.bind(this);
+    }
 
-	enable() {
-		this.setState({enabled: true});
-	}
-	
-	disable() {
-		this.setState({enabled: false});
-	}
+    enable() {
+        this.setState({ enabled: true });
+    }
 
-	render() {
-		if (this.state.enabled===false) {
-			return (
-				<div className='not-loading'>
-					&nbsp;
-				</div>
-			)
-		} else {
-			return (
-				<div className='loading-page-container'>
-					<div className="loading-frame">
-						<div className="loading-spin">
-							&nbsp;
-						</div>
-						<span>
-							{this.state.text}
-						</span>
-					</div>
-				</div>
-			)
-		}
+    disable() {
+        this.setState({ enabled: false });
+    }
 
-	}
+    render() {
+        if (this.state.enabled === false) {
+            return <div className="not-loading">&nbsp;</div>;
+        } else {
+            return (
+                <div className="loading-page-container">
+                    <div className="loading-frame">
+                        <div className="loading-spin">&nbsp;</div>
+                        <span>{this.state.text}</span>
+                    </div>
+                </div>
+            );
+        }
+    }
 }
 
 export default LoadingPage;
-
--- frontend/src/SubjectImage.js
+++ frontend/src/SubjectImage.js
@@ -1,24 +1,31 @@
-import React from 'react'; 
+import React from "react";
 
 class SubjectImage extends React.Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {url: props.url, lon: props.lon, lat: props.lat, ID: props.ID, PJ: props.PJ, style: props.style };
-		
-	}
+        this.state = {
+            url: props.url,
+			metadata: props.metadata,
+            style: props.style,
+        };
+    }
 
-	render() {
-		return (
-			<span key={this.state.ID+"_span"} style={this.state.style}>
-				<a href={"/subject/"+this.state.ID} target="_blank"  rel="noreferrer">
-					<img key={this.state.ID+"_img"} src={this.state.url} 
-						title={"lon: " + this.state.lon + " lat: " + this.state.lat}	
-						alt={"lon: " + this.state.lon + " lat: " + this.state.lat} className='subject-image' />
-				</a>
-			</span>
-		)
-	}
+    render() {
+        return (
+            <span key={this.state.ID + "_span"} style={this.state.style}>
+                <a href={"/subject/" + this.state.ID} target="_blank" rel="noreferrer">
+                    <img
+                        key={this.state.metadata.subject_ID + "_img"}
+                        src={this.state.url}
+                        title={"lon: " + this.state.metadata.longitude + " lat: " + this.state.metadata.latitude}
+                        alt={"lon: " + this.state.metadata.longitude + " lat: " + this.state.metadata.latitude}
+                        className="subject-image"
+                    />
+                </a>
+            </span>
+        );
+    }
 }
 
 export default SubjectImage;
